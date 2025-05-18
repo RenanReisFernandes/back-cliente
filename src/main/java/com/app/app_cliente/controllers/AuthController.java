8a50a43af2b6aa68a1a0ff4bdf8a7fef49aa21a5
Renan Reis <renanmacfernandes@hotmail.com>
2025-05-18 14:43:00 -0300

    AuthController

--- src/main/java/com/app/app_cliente/controllers/AuthController.java
+++ src/main/java/com/app/app_cliente/controllers/AuthController.java
@@ -1,6 +1,10 @@
 package com.app.app_cliente.controllers;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +23,16 @@ public class AuthController {
 	@PostMapping("/login")
 	public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request){
 		
+		UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword());
+		Authentication authenticate = AuthenticationManager.authenticate(userAndPass);
+		
+		User user = (User) authenticate.getPrincipal();
+		String token = tokenService.generateToken(user);
+		
+		return ResponseEntity.ok(AuthResponse.builder()
+				.accessToken(token)
+				.nome(user.getName())
+				.build());
 	}
 
 }
