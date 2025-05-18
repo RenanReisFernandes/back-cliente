a531c1c4e63d627ee2e906cdf9796d5b7835d2c6
Renan Reis <renanmacfernandes@hotmail.com>
2025-05-18 03:50:40 -0300

    RegisterUserRequest creation

---
+++ src/main/java/com/app/app_cliente/controllers/RegisterController.java
@@ -0,0 +1,13 @@
+package com.app.app_cliente.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/register")
+public class RegisterController {
+
+}
---
+++ src/main/java/com/app/app_cliente/request/RegisterUserRequest.java
@@ -0,0 +1,14 @@
+package com.app.app_cliente.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserRequest {
+	
+	private String nome;
+	private String email;
+	private String senha;
+
+}
