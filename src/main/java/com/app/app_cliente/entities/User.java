f9137f28f797fbcd49b86de151032d28f686f833
Renan Reis <renanmacfernandes@hotmail.com>
2025-05-18 04:29:11 -0300

    registration

--- src/main/java/com/app/app_cliente/entities/User.java
+++ src/main/java/com/app/app_cliente/entities/User.java
@@ -14,14 +14,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Builder
+@Table(name = "USER_DB")
 @Getter
 @Setter
 @AllArgsConstructor
--- src/main/java/com/app/app_cliente/request/RegisterUserRequest.java
+++ src/main/java/com/app/app_cliente/request/RegisterUserRequest.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class RegisterUserRequest {
-	
+
 	private String nome;
 	private String email;
 	private String senha;
